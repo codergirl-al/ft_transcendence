6ad963e0d7e6bc46cb3e8c8f5e05324d01ef5883
Albina Peposhi <codegirl.al@gmail.com>
2025-03-28 18:05:55 +0100

    cleanup of old routing

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -2,12 +2,13 @@ import { FastifyInstance, FastifyRequest, FastifyReply,  } from "fastify";
 import { apiRoutes } from "./apiRoutes";
 import { spa } from "../controllers/root.controller";
 import { setFastifyInstance, loginPage, callback } from "../controllers/login.controller";
-import { newGameForm } from "../controllers/game.controller";
+// import { newGameForm } from "../controllers/game.controller";
 import path from "path";
 // import path from "node:path";
 // import fastifyStatic from "@fastify/static";
 // import { createProfile, addNewProfile, showProfile, setFastifyInstance, userLogin, loginPage, callback } from "../controllers/login.controller.js";
 import { newGameForm, newSingleGameForm } from "../controllers/game.controller";
+
 
 export default async function routes(fastify: FastifyInstance) {
 	setFastifyInstance(fastify);
@@ -19,8 +20,8 @@ export default async function routes(fastify: FastifyInstance) {
 	//   });
 	  
 	fastify.get("/google-login/callback", callback);
-	fastify.get('/game/new/multi', newGameForm);
-	fastify.get('/game/new/single', newSingleGameForm);
+	// fastify.get('new?mode=multi', newGameForm);
+	// fastify.get('/new?mode=single', newSingleGameForm);
 	// fastify.post("/api/game/", newGame);
 	fastify.register(apiRoutes, { prefix: "/api" });
 
