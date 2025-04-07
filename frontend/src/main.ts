48ff3fbfbb44c29b217d694f6bfeb1f7dbf814e6
sshahary <shaharyarnawaz9@gmail.com>
2025-04-07 09:30:55 +0200

    deleted extra stuff from my piece

--- frontend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { navigateTo, loadPage } from "./router";
-import "./styles/tailwind.css";
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadPage(window.location.pathname);
-  window.onpopstate = () => loadPage(window.location.pathname);
-});
-
